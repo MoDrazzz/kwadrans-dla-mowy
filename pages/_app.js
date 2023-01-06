d83a8361df0dca7485c3f5d99093c7b19a0f7876
Grzegorz Pawlik <grzegorz8.p@gmail.com>
2023-01-06 16:19:08 +0100

    Add content fetching from CMS

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
 import { ThemeProvider } from 'styled-components';
-import GlobalStyle from '../styles/GlobalStyle';
-import '../styles/globals.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import theme from 'styles/theme';
+import GlobalStyle from 'styles/GlobalStyle';
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
